e34e74a54b7c22771286085e8507a1c24571c06c
William <willtheorangeguy@users.noreply.github.com>
2022-05-16 08:27:28 -0700

    Move to a function

--- main.py
+++ main.py
@@ -20,39 +20,42 @@ from tkinter import *
 
 # Build Window
 window = Tk()
-window.title("Online Account Avatar and Banners")
+window.title("Online Account Avatars and Banners")
 
 # Window Title 
-title = Label(window, text = "Online Account Avatar and Banners")
-
-# Account Avatars
-account1 = Frame(window)
-img_account1 = PhotoImage(file = "PyAvatar/images/placeholder.gif") # needs to be a GIF
-title_account1 = Label(account1, image = img_account1)
-text_account1 = Label(account1, text = "Account #1")
-
-account2 = Frame(window)
-img_account2 = PhotoImage(file = "PyAvatar/images/placeholder.gif") # needs to be a GIF
-title_account2 = Label(account2, image = img_account2)
-text_account2 = Label(account2, text = "Account #2")
-
-account3 = Frame(window)
-img_account3 = PhotoImage(file = "PyAvatar/images/placeholder.gif") # needs to be a GIF
-title_account3 = Label(account3, image = img_account3)
-text_account3 = Label(account3, text = "Account #1")
-
-account4 = Frame(window)
-img_account4 = PhotoImage(file = "PyAvatar/images/placeholder.gif") # needs to be a GIF
-title_account4 = Label(account4, image = img_account4)
-text_account4 = Label(account4, text = "Account #1")
-
-account5 = Frame(window)
-img_account5 = PhotoImage(file = "PyAvatar/images/placeholder.gif") # needs to be a GIF
-title_account5 = Label(account5, image = img_account5)
-text_account5 = Label(account5, text = "Account #1")
-
-# Window Layout
+title = Label(window, text = "Online Account Avatars and Banners")
 title.grid(row = 1, column = 1, columnspan = 5)
+
+# Account Avatar Function
+row_count = 2
+column_count = 1
+def accounts(image, name, hyperlink):
+    global row_count
+    global column_count
+
+    # Build each account frame
+    account = Frame(window)
+    img_account = PhotoImage(file = image) # needs to be a GIF
+    title_account = Label(account, image = img_account)
+    text_account = Label(account, text = name)
+    hyperlink_account = Label(account, text = hyperlink)
+
+    # Layout elements in frame
+    title_account.pack(side = TOP)
+    text_account.pack(side = BOTTOM)
+    hyperlink_account.pack(side = BOTTOM)  
+
+    # Layout elements in window
+    if column_count <= 5:
+        account.grid(row = row_count, column = column_count, padx= 5, pady= 5)
+        column_count += 1
+    else:
+        row_count += 1
+        column_count = 1
+        account.grid(row = row_count, column = column_count, padx = 5, pady = 5)
+
+# Account Profiles
+accounts("PyAvatar/images/placeholder.gif", "Github", "https://github.com")
 
 # Sustain Window
 window.mainloop()
