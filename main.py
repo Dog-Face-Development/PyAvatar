9084a6620fb64dc4f38b57b6ae8547e58efcf05e
Restyled.io <commits@restyled.io>
2023-01-03 12:45:15 +0000

    Restyled by reorder-python-imports

--- main.py
+++ main.py
@@ -14,10 +14,9 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-
 import webbrowser
-# Import Statements
 from tkinter import *
+# Import Statements
 
 # Account Avatar Function
 row_count = 2
