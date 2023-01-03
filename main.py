520b4cd98938d9e42d30dc14b68c9551d25145ac
Restyled.io <commits@restyled.io>
2023-01-03 12:45:14 +0000

    Restyled by isort

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
+import webbrowser
 # Import Statements
 from tkinter import *
-import webbrowser
 
 # Account Avatar Function
 row_count = 2
